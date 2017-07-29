019997e785ffdde3bb596e03b7f7f79b5fd2a62f
Swapnil Kumbhar <swapsk05@gmail.com>
2017-07-29 23:57:06 +0530

    Killed myself very slowly

--- db/ceque.sql
+++ db/ceque.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 29, 2017 at 07:37 PM
+-- Generation Time: Jul 29, 2017 at 08:25 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -32,8 +32,8 @@ CREATE TABLE `feedback_lp` (
   `fb_id` bigint(100) NOT NULL,
   `sme_id` bigint(100) NOT NULL,
   `lp_id` bigint(100) NOT NULL,
-  `feedback` blob NOT NULL,
-  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `feedback` text CHARACTER SET utf8 NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -47,7 +47,7 @@ CREATE TABLE `feedback_video` (
   `sme_id` bigint(100) NOT NULL,
   `video_id` bigint(100) NOT NULL,
   `feedback` blob NOT NULL,
-  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -63,6 +63,7 @@ CREATE TABLE `learning_program` (
   `lp_subject` varchar(50) NOT NULL,
   `lp_grade` varchar(50) NOT NULL,
   `lp_language` varchar(50) NOT NULL,
+  `lp_desc` text CHARACTER SET utf8 NOT NULL,
   `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -88,18 +89,10 @@ CREATE TABLE `user` (
   `user_firstname` varchar(100) NOT NULL,
   `user_lastname` varchar(100) NOT NULL,
   `user_email` varchar(50) NOT NULL,
-  `user_phone` bigint(15) NOT NULL,
+  `user_phone` int(15) NOT NULL,
   `user_city` varchar(50) NOT NULL,
   `user_password` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `user_firstname`, `user_lastname`, `user_email`, `user_phone`, `user_city`, `user_password`) VALUES
-(1, 'Swapnil', 'Kumbhar', 'swapsk05@gmail.com', 8082022984, 'Mumbai', 'lolboi'),
-(2, 'S', 'K', 'swapnil', 1234567890, 'Mumbai', 'lolboi');
 
 -- --------------------------------------------------------
 
@@ -108,8 +101,8 @@ INSERT INTO `user` (`user_id`, `user_firstname`, `user_lastname`, `user_email`, 
 --
 
 CREATE TABLE `user_roles` (
-  `user_id` bigint(100) NOT NULL,
-  `role_id` bigint(100) DEFAULT NULL
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -121,7 +114,7 @@ CREATE TABLE `user_roles` (
 CREATE TABLE `video` (
   `video_id` bigint(100) NOT NULL,
   `lp_id` bigint(100) NOT NULL,
-  `video_link` varchar(100) NOT NULL,
+  `video_link` varchar(100) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
   `video_title` varchar(50) NOT NULL,
   `teacher_id` bigint(20) NOT NULL,
   `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -136,23 +129,23 @@ CREATE TABLE `video` (
 --
 ALTER TABLE `feedback_lp`
   ADD PRIMARY KEY (`fb_id`),
-  ADD UNIQUE KEY `sme_id` (`sme_id`),
-  ADD UNIQUE KEY `lp_id` (`lp_id`);
+  ADD KEY `sme_id` (`sme_id`),
+  ADD KEY `lp_id` (`lp_id`);
 
 --
 -- Indexes for table `feedback_video`
 --
 ALTER TABLE `feedback_video`
   ADD PRIMARY KEY (`fb_id`),
-  ADD UNIQUE KEY `sme_id` (`sme_id`),
-  ADD UNIQUE KEY `video_id` (`video_id`);
+  ADD KEY `sme_id` (`sme_id`),
+  ADD KEY `video_id` (`video_id`);
 
 --
 -- Indexes for table `learning_program`
 --
 ALTER TABLE `learning_program`
   ADD PRIMARY KEY (`lp_id`),
-  ADD UNIQUE KEY `teacher_id` (`teacher_id`);
+  ADD KEY `teacher_id` (`teacher_id`);
 
 --
 -- Indexes for table `roles`
@@ -170,16 +163,17 @@ ALTER TABLE `user`
 -- Indexes for table `user_roles`
 --
 ALTER TABLE `user_roles`
-  ADD UNIQUE KEY `user_id` (`user_id`),
-  ADD UNIQUE KEY `role_id` (`role_id`);
+  ADD KEY `role_id` (`role_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `video`
 --
 ALTER TABLE `video`
   ADD PRIMARY KEY (`video_id`),
-  ADD UNIQUE KEY `lp_id` (`lp_id`),
-  ADD UNIQUE KEY `teacher_id` (`teacher_id`);
+  ADD KEY `lp_id` (`lp_id`),
+  ADD KEY `teacher_id` (`teacher_id`),
+  ADD KEY `lp_id_2` (`lp_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -209,7 +203,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` bigint(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` bigint(100) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `video`
 --
@@ -230,8 +224,8 @@ ALTER TABLE `feedback_lp`
 -- Constraints for table `feedback_video`
 --
 ALTER TABLE `feedback_video`
-  ADD CONSTRAINT `feedback_video_ibfk_1` FOREIGN KEY (`sme_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `feedback_video_ibfk_2` FOREIGN KEY (`video_id`) REFERENCES `video` (`video_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `feedback_video_ibfk_1` FOREIGN KEY (`video_id`) REFERENCES `video` (`video_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `feedback_video_ibfk_2` FOREIGN KEY (`sme_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `learning_program`
