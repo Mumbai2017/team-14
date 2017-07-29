fea8eeaf03cc2f3a7422d44d5a05b4fd8ddc0bea
Soniya Rode <soniarode67@yahoo.in>
2017-07-29 18:47:01 +0530

    feedback
    feedback folder created

---
+++ service/feedback/feedbackLessonPlan.php
@@ -0,0 +1,32 @@
+
+ <?php
+ require_once '../connection.php';
+ if(isset($_POST["comment"])&&isset($_POST["feedback"])){
+     //Required Inputs
+     $feedback = $con->real_escape_string($$_POST["feedback"]);
+     $lp_id = $con->real_escape_string($$_POST["lp_id"]);
+     $sme_id = $con->real_escape_string($$_POST["sme_id"]);
+     //storing data to the db
+        $sql="INSERT INTO feedback_lp(sme_id,lp_id,feedback,ts) values ($sme_id."','".$lp_id."','".$message.",NOW());
+		
+     $stmt = $con->prepare($sql);
+     if($stmt){
+        
+             if($stmt->execute()){
+                
+                    $output["code"] = 1;
+                    $outpu["msg"]="feedback submitted";
+                }
+             }
+         
+     
+ }
+ else{
+     $output["code"] = 2;
+     $output["msg"] = "Invalid Data";
+     echo json_encode($output);
+
+ }
+?>
+
+
