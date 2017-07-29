aadca792f2acdeaf89fa5ec1ba3b840044a1e5ef
franky-naidu <frankynaidu16081996@gmail.com>
2017-07-30 02:17:14 +0530

    sdf

--- service/login/auth.php
+++ service/login/auth.php
@@ -3,7 +3,7 @@
  if(isset($_POST["username"])&&isset($_POST["password"])) {
      $name = $con->real_escape_string($_POST["username"]);
      $password = $con->real_escape_string($_POST["password"]);
-     $sql = "select * from user where `user_email` like  '$name' and `user_password` like '$password' ";
+     $sql = "select * from user where `user_email` like  '$name' and `user_password` = '$password' ";
     if($result =  $con->query($sql)){
         if($result->num_rows > 0){
             $ouput["code"] = 1;
@@ -23,15 +23,16 @@
             echo json_encode($output);
         }
         else{
-            $ouput["code"] = 2;
+            $ouptut["code"] = 2;
             $output["msg"] = "NOt Authenticated";
             echo json_encode($output);
         }
     }
-     $ouput["code"] = 2;
-     $output["msg"] = "Try Later";
-     echo json_encode($output);
-
+    else {
+        $output["code"] = 2;
+        $output["msg"] = "Try Later";
+        echo json_encode($output);
+    }
  }
  else{
      $ouput["code"] = 2;
