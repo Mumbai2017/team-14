5e84aa2684020a59b1f2f3fbc20abb810fbb81de
franky-naidu <frankynaidu16081996@gmail.com>
2017-07-29 16:27:42 +0530

    Login Service

--- service/connection.php
+++ service/connection.php
@@ -1,11 +1,11 @@
 <?php
- const SERVERNAME = "";
+ const SERVERNAME = "10.49.166.0";
  const USERNAME="";
  const PASSWORD="";
  const  DATABASE="";
 
  $con = new mysqli(SERVERNAME,USERNAME,PASSWORD,DATABASE);
-
+$con->autocommit(false);
  if($con->error){
      die("Connection Error");
  }
--- service/login/auth.php
+++ service/login/auth.php
@@ -1,13 +1,31 @@
 <?php
- require_once 'connection.php';
+ require_once '../connection.php';
  if(isset($_POST["username"])&&isset($_POST["password"])){
+     //REquired Inputs
      $name = $con->real_escape_string($$_POST["username"]);
      $password = $con->real_escape_string($$_POST["username"]);
+     $sql = "select * from user where `user_email` like ? and `user_password` like ?";
 
-     $stmt = "";
+     $stmt = $con->prepare($sql);
+     if($stmt){
+         if($stmt->bind_param("ss",$name,$pasword)){
+             if($stmt->execute()){
+                if($stmt->num_rows >0){
+                    $output["code"] = 1;
+                    $outpu["ms  g"]="Authenticated";
+                }
+             }
+         }
+
+
+     }
+
  }else{
      $output["code"] = 2;
      $output["msg"] = "Invalid Data";
      echo json_encode($output);
  }
 ?>
+
+//
+
